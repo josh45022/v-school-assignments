2518699a2be81240fcbc5dd8a39cb3dcbbf57f2a
Joshua Benbaba <joshuabenbaba@Marys-MacBook-Pro-3.local>
2020-10-29 10:55:56 -0600

    Finished Mom's Shopping List

--- Projects/Module 1/moms-shopping-list-start/index.js
+++ Projects/Module 1/moms-shopping-list-start/index.js
@@ -3,19 +3,19 @@ var form = document["form1"];
 
 
 form.addEventListener("submit", (event) => {
+   
     event.preventDefault ()
     var item = form.itemChoice.value;
     form.itemChoice.value = "";
 
     var h4 = document.createElement('h4');
     h4.textContent = item;
-    document.getElementsByTagName("body")[0].append(h4)
-})
+    document.getElementById("list-of-items").append(h4)
 
-var deleter = document.querySelector(".delete");
-
-document.deleter.addEventListener("click", (event)=> {
-    event.preventDefault ()
-    h4.textContent = " ";
-    console.log(event.delete);
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+    h4.append(deleteButton);
+    deleteButton.addEventListener("click", (event)=>{
+    h4.remove(deleteButton)
+    })
 })
